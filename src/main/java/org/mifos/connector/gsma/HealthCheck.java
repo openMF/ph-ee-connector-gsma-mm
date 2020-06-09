a39af4970c7ebb0cef03c0e9c9de58114fd75d40
SubhamPramanik <subhampramanik@gmail.com>
2020-06-10 01:13:42 +0530

    dev: added API for zeebe test

--- src/main/java/org/mifos/connector/gsma/HealthCheck.java
+++ src/main/java/org/mifos/connector/gsma/HealthCheck.java
@@ -2,10 +2,15 @@ package org.mifos.connector.gsma;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.mifos.connector.gsma.zeebe.ZeebeProcessStarter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HealthCheck extends RouteBuilder {
+
+    @Autowired
+    public ZeebeProcessStarter zeebeProcessStarter;
 
     @Override
     public void configure() {
@@ -17,5 +22,15 @@ public class HealthCheck extends RouteBuilder {
                 .to("direct:get-access-token")
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
                 .setBody(simple("${body}"));
+
+        from("rest:GET:/deploy")
+                .to("direct:test-bpmn")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
+
+        from("direct:test-bpmn")
+                .id("test-bpmn")
+                .process(exchange -> {
+                    zeebeProcessStarter.startZeebeWorkflow("sampleProcess");
+                });
     }
 }
